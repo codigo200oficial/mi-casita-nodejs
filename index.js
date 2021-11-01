92eb1ae411c7addbf0dc834c7a99808b615b24c4
Codigo200 <codigo.200.oficial@gmail.com>
2021-11-01 11:55:23 -0500

    Agregrando ventanas.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const colors = require("colors")
 const paredes = require("./src/paredes");
 const puerta = require("./src/puerta");
 const techo = require("./src/techo");
+const ventanas = require("./src/ventanas");
 
 
 console.log("PROYECTO INICIADO..".red);
 paredes();
 puerta();
 techo();
+ventanas();
